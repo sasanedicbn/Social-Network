73f69aac0abd6b88b50e486e581fa12ab8523571
Sasa Nedic <svetasmak0@gmail.com>
2024-02-06 22:07:44 +0100

    Create posts data

--- Social-network/data.js
+++ Social-network/data.js
@@ -50,4 +50,97 @@ export const user = {
       img: "/assets/arslan6.jpg",
     },
   ],
+
+  posts: [
+    {
+      id: crypto.randomUUID(),
+      postText: "Believe you can and you're halfway there.",
+      postDate: "2 days ago",
+      likes: [
+        {
+          name: "Isak",
+          lastName: "Mihajlovic",
+        },
+        {
+          name: "Nikolina",
+          lastName: "Ikic",
+        },
+      ],
+      comments: [
+        {
+          name: "Isak",
+          lastName: "Mihajlovic",
+          commentText: "Wow, this is amazing",
+          img: "/assets/arslan6.jpg",
+        },
+        {
+          name: "Jovan",
+          lastName: "Jovic",
+          commentText: "Absolutely love this quote!",
+          img: "/assets/dobrinko3.webp",
+        },
+        {
+          name: "Angelina",
+          lastName: "Jolie",
+          commentText:
+            "Sometimes, it's the small gestures that make someone a hero",
+          img: "/assets/Angelina-Jolie.webp",
+        },
+      ],
+    },
+    {
+      id: crypto.randomUUID(),
+      postText:
+        "The only limit to our realization of tomorrow will be our doubts of today.",
+      postDate: "7 days ago",
+      likes: [
+        {
+          name: "Dobrila",
+          lastName: "Jovina",
+        },
+      ],
+      comments: [],
+    },
+    {
+      id: crypto.randomUUID(),
+      postText: "Don't watch the clock; do what it does. Keep going.",
+      postDate: "1 day ago",
+      likes: [
+        { name: "Jovan", lastName: "Jovic" },
+        { name: "Angelina", lastName: "Jolie" },
+        { name: "Isak", lastName: "Mihajlovic" },
+        { name: "Milan", lastName: "Samardzic" },
+      ],
+      comments: [
+        {
+          name: "Isak",
+          lastName: "Mihajlovic",
+          commentText:
+            "Sometimes the choices we face are so tough, it's almost like navigating through shades of gray.",
+          img: "/assets/arslan6.jpg",
+        },
+        {
+          name: "Angelina",
+          lastName: "Jolie",
+          commentText:
+            " Choosing between evils can be a dilemma, but your stance adds a layer of wisdom to it. ",
+          img: "/assets/Angelina-Jolie.webp",
+        },
+        {
+          name: "Isak",
+          lastName: "Mihajlovic",
+          commentText:
+            " It's a reminder that sometimes the best option is to stay true to your principles, even if the choices seem challenging.",
+          img: "/assets/arslan6.jpg",
+        },
+        {
+          name: "Milan",
+          lastName: "Samardzic",
+          commentText:
+            "Makes me ponder on the importance of staying true to one's moral compass. ",
+          img: "/assets/cetvrti.jpg",
+        },
+      ],
+    },
+  ],
 };
