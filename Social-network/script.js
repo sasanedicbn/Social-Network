131bcb4a3ab56b65ea067cdae191d45eee8f1150
Sasa Nedic <svetasmak0@gmail.com>
2024-02-07 01:54:26 +0100

    Display data on screen

--- Social-network/script.js
+++ Social-network/script.js
@@ -3,5 +3,15 @@ const friendsList = document.querySelector(".friend__list");
 const friends = user.friends;
 friends.forEach((friend) => {
   const listItem = document.createElement("li");
+  listItem.innerHTML = `
+  
+    <div class="friends-data">
+      <img src="${friend.img}" alt="${friend.name}"/>
+      <p class="friends-name">
+        ${friend.name}  ${friend.lastName}
+      </p>
+    </div>
+  `;
+  friendsList.appendChild(listItem);
 });
 console.log(friends);
