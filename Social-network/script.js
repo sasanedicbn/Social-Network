e44beaee5709b5ba0224a5ff25df412a4331957c
Sasa Nedic <svetasmak0@gmail.com>
2024-02-08 21:01:43 +0100

    Create method for rendering posts

--- Social-network/script.js
+++ Social-network/script.js
@@ -24,6 +24,47 @@ class Post {
 }
 class Friends {
   constructor() {}
+  redeneringPosts() {
+    const postsContainer = document.querySelector(".posts");
+    const posts = user.posts;
+
+    posts.forEach((post) => {
+      const postElement = document.createElement("div");
+      postElement.classList.add("post");
+
+      postElement.innerHTML = `
+    <img src="${post.img}" alt="${post.img}" class="user-img" />
+    <div class="user-time">
+    <span class="user-profile">Sasa Nedic</span>
+    <p class="post-date">${post.postDate}</p>
+    </div>
+    <p class="post-text">${post.postText}</p>
+
+    <div class="likes-comments-container">
+      <p class="likes"><span class="like-emoji"><ion-icon name="thumbs-up-outline"></ion-icon></span> ${post.likes.length} Likes</p>
+      <p class="comments">${post.comments.length} Comments</p>
+    </div>
+  `;
+
+      post.comments.forEach((comment) => {
+        const commentElement = document.createElement("div");
+        commentElement.classList.add("comment");
+
+        commentElement.innerHTML = `
+      
+      <div class="comment-info">
+        <img src="${comment.img}" alt="${comment.name}" />
+        <p>${comment.name} ${comment.lastName}</p>
+      </div> <br>
+      <p class="comment-text">${comment.commentText}</p>
+    `;
+
+        postElement.appendChild(commentElement);
+      });
+
+      postsContainer.appendChild(postElement);
+    });
+  }
 }
 
 // console.log(friends);
