537e27e04ce7a47ce4d40ef9fc9a9bdc087404a1
Sasa Nedic <svetasmak0@gmail.com>
2024-02-08 21:02:03 +0100

    Clean code

--- Social-network/script.js
+++ Social-network/script.js
@@ -67,26 +67,6 @@ class Friends {
   }
 }
 
-// console.log(friends);
-
-// class Post {
-//   constructor(post) {
-//     this.post = post;
-//   }
-//   likePost(like) {
-//     this.post.likes.push(like);
-//   }
-// }
-// const POST = new Post(user);
-// console.log(POST.likePost());
-// console.log(POST.post.posts);
-// const likes = document.querySelector(".likes");
-// const postsAll = POST.post.posts;
-// likes.addEventListener("click", function (event) {
-//   postsAll.forEach((post) => console.log(post));
-//   console.log("ziv");
-// });
-
 // classa user
 //kad prodje setTim priakzati
 //class visitingprofile fname lname
